34aa5211461c3452de5f442d2499a478d4fe00ac
mehkij <disismehki@gmail.com>
2025-03-01 01:51:16 -0500

    Configure Vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: "index.html",
         omnibox: "src/omnibox.ts",
+        settings: "src/pages/Settings/settings.html",
       },
       output: {
         entryFileNames: "[name].js",
