b1b183f9e1e7e962d96c7cb49684a910b16623a6
mehkij <disismehki@gmail.com>
2025-03-01 00:18:34 -0500

    Configure Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        omnibox: "src/omnibox.ts",
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
 });
