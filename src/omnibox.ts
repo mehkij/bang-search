db936e460bafc7201ce573657eba1dbca95fda1f
mehkij <disismehki@gmail.com>
2025-03-01 01:36:42 -0500

    Fix extension opening new tab

--- src/omnibox.ts
+++ src/omnibox.ts
@@ -7,20 +7,20 @@ function querySearch(text: string) {
   // If bang not used in first keyword, default to a Google search.
   if (!query[0].includes("!")) {
     fullURL = "https://google.com/search?q=" + query.slice(1).join("+");
-    chrome.tabs.create({ url: fullURL });
+    chrome.tabs.update({ url: fullURL });
   } else {
     try {
       chrome.storage.sync.get(["savedPairs"], (result: StorageResult) => {
         const pairs = result.savedPairs;
         fullURL = pairs[query[0]].url + query.slice(1).join("+");
         console.log("Full URL in try block: ", fullURL);
-        chrome.tabs.create({ url: fullURL });
+        chrome.tabs.update({ url: fullURL });
         return;
       });
     } catch (err) {
       fullURL = "https://google.com/search?q=" + query.slice(1).join("+");
       console.error("Failed to find pair: ", err);
-      chrome.tabs.create({ url: fullURL });
+      chrome.tabs.update({ url: fullURL });
     }
   }
 }
