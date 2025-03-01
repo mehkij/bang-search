1cc2959bd0bdc49cf63506d0aebc366a2b75c8df
mehkij <disismehki@gmail.com>
2025-03-01 00:17:24 -0500

    Add omnibox functionality

---
+++ src/omnibox.ts
@@ -0,0 +1,28 @@
+import StorageResult from "./types/StorageResult";
+
+function querySearch(text: string) {
+  const query: string[] = text.split(" ");
+  let fullURL: string = "";
+
+  // If bang not used in first keyword, default to a Google search.
+  if (!query[0].includes("!")) {
+    fullURL = "https://google.com/search?q=" + query.slice(1).join("+");
+    chrome.tabs.create({ url: fullURL });
+  } else {
+    try {
+      chrome.storage.sync.get(["savedPairs"], (result: StorageResult) => {
+        const pairs = result.savedPairs;
+        fullURL = pairs[query[0]].url + query.slice(1).join("+");
+        console.log("Full URL in try block: ", fullURL);
+        chrome.tabs.create({ url: fullURL });
+        return;
+      });
+    } catch (err) {
+      fullURL = "https://google.com/search?q=" + query.slice(1).join("+");
+      console.error("Failed to find pair: ", err);
+      chrome.tabs.create({ url: fullURL });
+    }
+  }
+}
+
+chrome.omnibox.onInputEntered.addListener(querySearch);
